6e3279b4f7e7266bfa90ef8f16d5d23111b20eeb
Ian Babrou <ibobrik@gmail.com>
2014-07-26 21:33:47 +0400

    added size limit

--- make.sh
+++ make.sh
@@ -2,19 +2,21 @@
 
 set -e
 
-if [ -z $3 ]; then
-    echo "usage: ./make.sh share_name login password"
+if [ -z $4 ]; then
+    echo "usage: ./make.sh <share_name> <login> <password> <max_size_in_mb>"
     exit 1
 fi
 
 NAME=$1
 LOGIN=$2
 PASSWORD=$3
+MAX_SIZE=$4
 
 cat Dockerfile.template | \
     sed "s/%name%/${NAME}/g" | \
     sed "s/%login%/${LOGIN}/g" | \
-    sed "s/%password%/${PASSWORD}/g" \
+    sed "s/%password%/${PASSWORD}/g" | \
+    sed "s/%max_size%/${MAX_SIZE}/g" \
     > Dockerfile
 
 LOWER_NAME=$(echo "${NAME}" | tr '[:upper:]' '[:lower:]')
@@ -24,4 +26,4 @@ IMAGE="time-container-${LOWER_NAME}"
 echo "building image ${IMAGE}"
 docker build -t ${IMAGE} .
 
-echo ">> run: docker run -d -p <docker_host_ip>:548:548 -v <docker_host_dir>:/share time-container-<share_name>"
+echo ">> run: docker run -d -p <docker_host_ip>:548:548 -v <docker_host_dir>:/share time-container-${LOWER_NAME}"
